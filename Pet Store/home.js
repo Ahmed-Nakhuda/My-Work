4f6442f61b42b354fc1189576dbdad0ebdd5bbdf
Ahmed-Nakhuda <144385153+Ahmed-Nakhuda@users.noreply.github.com>
2024-03-24 13:13:44 -0400

    Update home.js

--- Pet Store/home.js
+++ Pet Store/home.js
@@ -1,8 +1,3 @@
-/* "StAuth10244: I Ahmed Nakhuda, 000878456 certify that this material is my original work. No
-other person's work has been used without due acknowledgement. I have not made
-my work available to anyone else."
-*/
-
 import '../App.css';
 
 function Home() {
